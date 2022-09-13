3f82ecc676b1681c5b3dc751c13ec0a6587ba785
AhmetBoyraz01~ <ahmetboyraz.de@gmail.com>
2022-09-13 19:28:50 +0300

    Steps

---
+++ game.js
@@ -0,0 +1,23 @@
+//* =====================================
+//*  GUESS MY NUMBER
+//*  Game Logic
+//*======================================
+
+//? 1-100 arasinda rastgele bir sayi tut.
+
+//? CheckBtn basildiginda kontrolleri yap
+
+//? eger input girilmediyse
+//? kullaniciya uyari ver
+
+//! eger rastgele == input.value
+//? tebrikler bildiniz
+//? background = green
+//? eger score > topScore
+
+
+//! degilse
+//? eger rastgele sayi < input.value
+//? AZALT
+//? degilse
+//? ARTTIR
